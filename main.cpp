89d1de841d8ebff696281e8c11b911f6f1251f13
Nixxiliss <113019627+Nixxiliss@users.noreply.github.com>
2022-09-07 11:36:44 +0200

    Main.cpp
    This is a fake hacking menu in c++

---
+++ main.cpp
@@ -0,0 +1,269 @@
+//This is fake Hacking menu
+//Author  Nix
+ 
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "loading Program \n";
+    std::cout << "Please Wait. \n";
+
+    // loading screen start
+    {
+        float simulation_duration = 0.0;
+        float maximum_duration = 10.0;
+        auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+        do {
+
+            // Code for simulation
+            auto current_time = chrono::high_resolution_clock::now();
+            std::chrono::duration<double> time_since_last_asterisk =
+                    current_time - time_since_start_or_last_asterisk;
+            if (time_since_last_asterisk.count() >= 2.0) {
+                cout << "*";
+                cout.flush();
+                simulation_duration += time_since_last_asterisk.count();
+                time_since_start_or_last_asterisk = current_time;
+            }
+
+        } while (simulation_duration < maximum_duration);
+        cout << endl;
+    }
+    //loading screen end
+
+    std::cout << "Welcome to Pre Menu. \n";
+    std::cout << "To Start menu 1\n";
+
+//buy screen
+
+    int lvl = 0;
+    std::cin >> lvl;
+
+    int cost = 0;
+
+    if (lvl <= 1) {
+        cost = 1200;
+    }
+
+    std::cout << "Price for you is a " << cost << " dollars" << std::endl;
+    std::cout << "send eth on this wallet 0xf122ce797834255DA2C575c231796ec935B\n"; // fake wallet 
+    std::cout << "If not then I have your pc data and your location even vpn won't help :]\n"; // this is a fake informations 
+    std::cout << "Wait a seconds \n";
+    {
+        float simulation_duration = 0.0;
+        float maximum_duration = 10.0;
+        auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+        do {
+
+            // Code for simulation
+            auto current_time = chrono::high_resolution_clock::now();
+            std::chrono::duration<double> time_since_last_asterisk =
+                    current_time - time_since_start_or_last_asterisk;
+            if (time_since_last_asterisk.count() >= 2.0) {
+                cout << "*";
+                cout.flush();
+                simulation_duration += time_since_last_asterisk.count();
+                time_since_start_or_last_asterisk = current_time;
+            }
+
+        } while (simulation_duration < maximum_duration);
+        cout << endl;
+    }
+
+    //Main menu screen
+
+    std::cout << "Welcome to main menu:\n\n";
+    std::cout << "1)Hacking wlan:\n";
+
+    int selection = 0;
+    std::cout << "Select options: ";
+    std::cin >> selection;
+
+    switch (selection) {
+        case 1:
+            std::cout << "Password is a Hacking" << std::endl;
+            std::cout << "Wait" << std::endl;
+    }
+
+    {
+        float simulation_duration = 0.0;
+        float maximum_duration = 50.0;
+        auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+        do {
+
+            // Code for simulation
+            auto current_time = chrono::high_resolution_clock::now();
+            std::chrono::duration<double> time_since_last_asterisk =
+                    current_time - time_since_start_or_last_asterisk;
+            if (time_since_last_asterisk.count() >= 10.0) {
+                cout << "*";
+                cout.flush();
+                simulation_duration += time_since_last_asterisk.count();
+                time_since_start_or_last_asterisk = current_time;
+            }
+
+        } while (simulation_duration < maximum_duration);
+        cout << endl;
+    }
+    std::cout << "This password for hacking wifi:\n\n";
+    std::cout
+            << "Password is VWo3NDFqaDE0Mmg5N3NoNzgx:\n"; // code in base 64
+    std::cout << "Return to menu \n";
+    {
+        float simulation_duration = 0.0;
+        float maximum_duration = 10.0;
+        auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+        do {
+
+            // Code for simulation
+            auto current_time = chrono::high_resolution_clock::now();
+            std::chrono::duration<double> time_since_last_asterisk =
+                    current_time - time_since_start_or_last_asterisk;
+            if (time_since_last_asterisk.count() >= 2.0) {
+                cout << "*";
+                cout.flush();
+                simulation_duration += time_since_last_asterisk.count();
+                time_since_start_or_last_asterisk = current_time;
+            }
+
+        } while (simulation_duration < maximum_duration);
+        cout << endl;
+
+    }
+
+
+    std::cout << "Welcome to main menu:\n\n";
+    std::cout << "1)Create MeterPreter:\n";
+
+    std::cout << "Select options: ";
+    std::cin >> selection;
+
+    switch (selection) {
+
+        case 1:
+            std::cout << "Meterpreter is creating on your desktop" << std::endl;
+            std::cout << "Wait" << std::endl;
+            {
+                float simulation_duration = 0.0;
+                float maximum_duration = 50.0;
+                auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+                do {
+
+                    // Code for simulation
+                    auto current_time = chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> time_since_last_asterisk =
+                            current_time - time_since_start_or_last_asterisk;
+                    if (time_since_last_asterisk.count() >= 10.0) {
+                        cout << "*";
+                        cout.flush();
+                        simulation_duration += time_since_last_asterisk.count();
+                        time_since_start_or_last_asterisk = current_time;
+                    }
+
+                } while (simulation_duration < maximum_duration);
+                cout << endl;
+            }
+            std::cout << "MeterPreter is created on your desktop" << std::endl;
+            std::cout << "Loading main menu" << std::endl;
+            {
+                float simulation_duration = 0.0;
+                float maximum_duration = 10.0;
+                auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+                do {
+
+                    // Code for simulation
+                    auto current_time = chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> time_since_last_asterisk =
+                            current_time - time_since_start_or_last_asterisk;
+                    if (time_since_last_asterisk.count() >= 2.0) {
+                        cout << "*";
+                        cout.flush();
+                        simulation_duration += time_since_last_asterisk.count();
+                        time_since_start_or_last_asterisk = current_time;
+                    }
+
+                } while (simulation_duration < maximum_duration);
+                cout << endl;
+            }
+            break;
+    }
+
+    std::cout << "Welcome to main menu:\n\n";
+    std::cout << "1)View Others version This Hacker menu:\n";
+
+    std::cout << "Select options: ";
+    std::cin >> selection;
+
+    switch (selection) {
+
+        case 1:
+            std::cout << "Others version This Hacker menu" << std::endl;
+            std::cout << "Your version is a noob" << std::endl;
+            std::cout << "Next version is a haker price is a 2500 dollars" << std::endl;
+            std::cout << "Next version is a pro haker price is a 5000 dollars" << std::endl;
+            std::cout << "last Version is a Immortal haker price is a 10000 dollars" << std::endl;
+            std::cout << "To buy any version contact on my emile " << std::endl;  // Enter your emile
+            std::cout << "Return to menu " << std::endl;
+            {
+                float simulation_duration = 0.0;
+                float maximum_duration = 10.0;
+                auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+                do {
+
+                    // Code for simulation
+                    auto current_time = chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> time_since_last_asterisk =
+                            current_time - time_since_start_or_last_asterisk;
+                    if (time_since_last_asterisk.count() >= 2.0) {
+                        cout << "*";
+                        cout.flush();
+                        simulation_duration += time_since_last_asterisk.count();
+                        time_since_start_or_last_asterisk = current_time;
+                    }
+
+                } while (simulation_duration < maximum_duration);
+                cout << endl;
+            }
+
+
+            break;
+
+
+    }
+
+    std::cout << "Welcome to main menu:\n\n";
+    std::cout << "1)To end program write 1:\n";
+
+    std::cout << "Select options: ";
+    std::cin >> selection;
+
+    switch (selection) {
+
+        case 1:
+            std::cout << "Program is Closing" << std::endl;
+            {
+                float simulation_duration = 0.0;
+                float maximum_duration = 5.0;
+                auto time_since_start_or_last_asterisk = chrono::high_resolution_clock::now();
+                do {
+
+                    // Code for simulation
+                    auto current_time = chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> time_since_last_asterisk =
+                            current_time - time_since_start_or_last_asterisk;
+                    if (time_since_last_asterisk.count() >= 1.0) {
+                        cout << "*";
+                        cout.flush();
+                        simulation_duration += time_since_last_asterisk.count();
+                        time_since_start_or_last_asterisk = current_time;
+                    }
+
+                } while (simulation_duration < maximum_duration);
+                cout << endl;
+            }
+
+            break;
+    }
+}
